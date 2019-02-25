797a69ec6c79cb015e84b21da332dd9de9e509c3
dcjones1 <dcejjones@gmail.com>
2019-02-25 11:11:34 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,41 @@
-class CrewMember {}
+let crewMemberId = 0
+class CrewMember {
+  constructor (position, ship) {
+    this.position = position
+    this.id = ++crewMemberId
+    this.setShip(ship)
+  }
+
+  setShip (ship) {
+    if (ship) {
+      this.currentShip = ship
+      ship.crewMembers.push(this)
+    } else {
+      this.currentShip = 'Looking for a Rig'
+    }
+  }
+
+  chargePhasers () {
+    if (this.position === 'Gunner' && this.currentShip != 'Looking for a Rig') {
+       this.currentShip.phasersCharge = 'charged'
+    } else {
+      return 'had no effect'
+    }
+  }
+
+  engageWarpDrive () {
+    if (this.position === 'Pilot' && this.currentShip != 'Looking for a Rig') {
+       this.currentShip.warpDrive = 'engaged'
+    } else {
+      return 'had no effect'
+    }
+  }
+
+  setsInvisibility () {
+    if (this.position === 'Defender' && this.currentShip != 'Looking for a Rig') {
+       this.currentShip.cloaked = true
+    } else {
+      return 'had no effect'
+    }
+  }
+}
