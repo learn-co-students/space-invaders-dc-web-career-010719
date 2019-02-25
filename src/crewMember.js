e4d613fe05b8fc5e4f048c6d658f150d9f0d0030
mkay0961 <mkay0961@gmail.com>
2019-02-25 09:44:15 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,34 @@
-class CrewMember {}
+class CrewMember {
+
+  constructor(pos){
+    this.position = pos
+    this.currentShip = "Looking for a Rig"
+  }
+  chargePhasers(){
+    if (this.position == "Gunner" && this.currentShip != "Looking for a Rig"){
+
+      this.currentShip.phasersCharge = "charged"
+    }else{
+      return 'had no effect'
+    }
+
+  }
+  engageWarpDrive(){
+    if (this.position == "Pilot" && this.currentShip != "Looking for a Rig"){
+      this.currentShip.warpDrive = "engaged"
+    }else{
+      return 'had no effect'
+    }
+  }
+  setsInvisibility(){
+    if (this.position == "Defender" && this.currentShip != "Looking for a Rig"){
+      this.currentShip.cloaked = true
+    }else{
+      return 'had no effect'
+    }
+  }
+
+
+
+
+}
