1e71c849bc396696f5c38b53f15eee46e1221028
shannon-nabors <shannon.nabors@gmail.com>
2019-02-25 16:01:57 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,17 @@
-class Spaceship {}
+let ShipID = 0
+
+class Spaceship {
+  constructor(name, crew, phasers=5, shields=4, cloaked=false, warpDrive="disengaged", docked=true, phasersCharge="uncharged") {
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = cloaked
+    this.warpDrive = warpDrive
+    this.crew.length === 0 ? this.docked = true : this.docked = false
+    this.phasersCharge = phasersCharge
+    this.id = ++ShipID
+
+    this.crew.forEach(member => member.currentShip = this)
+  }
+}
