1566345b80167f33c8239ecd300db9d22ed7c454
Ben Yellin <byellindc@gmail.com>
2019-02-25 00:17:09 -0500

    done

--- src/crewMember.js
+++ src/crewMember.js
@@ -5,48 +5,28 @@ class CrewMember {
   }
 
   engageWarpDrive() {
-    // if (typeof this.currentShip !== 'object')
-    //   return 'had no effect'
-
-    // if (this.position !== 'pilot') return
-    // this.currentShip.warpDrive = 'engaged'
-
-    // console.log(this.position, this)
-    // if (typeof this.currentShip !== 'object') {
-    //   return 'had no effect'
-    // } else if (this.position === 'pilot') {
-    //   this.currentShip.warpDrive = 'engaged'
-    // }
-
-    return this.position === 'Pilot' && this.currentShip !== 'Looking for a Rig' ?
-    ( this.currentShip.warpDrive = 'engaged' ) : 'had no effect';
+    if (typeof this.currentShip !== 'object') {
+      return 'had no effect'
+    } else if (this.position === 'pilot') {
+      this.currentShip.warpDrive = 'engaged'
+    }
   }
 
   setsInvisibility() {
-    // if (this.position !== 'defender') return
-
-    // console.log(this.position, this)
-    // if (typeof this.currentShip !== 'object') {
-    //   return 'had no effect'
-    // } else if (this.position === 'defender') {
-    //   this.currentShip.invisibility = true
-    // }
-
-    return this.position === 'Defender' && this.currentShip !== 'Looking for a Rig' ?
-    ( this.currentShip.cloaked = true ) :
-    'had no effect';
+    if (typeof this.currentShip !== 'object') {
+      return 'had no effect'
+    } else if (this.position === 'defender') {
+      this.currentShip.invisibility = true
+    }
   }
 
   chargePhasers() {
-    // // if (this.position !== 'gunner') return
-    // console.log(this.position, this)
-    // if (typeof this.currentShip !== 'object') {
-    //   return 'had no effect'
-    // } else if (this.position === 'gunner') {
-    //   this.currentShip.phasersCharge = 'charged'
-    // }
-    return this.position === 'Gunner' && this.currentShip !== 'Looking for a Rig' ?
-    ( this.currentShip.phasersCharge = 'charged' ) :
-    'had no effect';
+    // if (this.position !== 'gunner') return
+    console.log(this.position, this)
+    if (typeof this.currentShip !== 'object') {
+      return 'had no effect'
+    } else if (this.position === 'gunner') {
+      this.currentShip.phasersCharge = 'charged'
+    }
   }
 }
