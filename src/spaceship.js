8d83fd4312cd071c1686bacfe9f9132397e6d5f9
Ben Yellin <byellindc@gmail.com>
2019-02-25 00:16:25 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,17 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+    this.crew = crew
+
+    this.phasers = phasers
+    this.shields = shields
+
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    this.phasersCharge = 'uncharged'
+
+    this.docked = crew.length ? false : true
+
+    crew.forEach(c => c.currentShip = this)
+  }
+}
